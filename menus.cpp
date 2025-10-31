fdb610cf925baa61d1b8d0222c48084afc2c418f
Kanax01 <162582100+Kanax01@users.noreply.github.com>
2025-10-31 16:17:42 -0400

    Update menus.cpp

--- menus.cpp
+++ menus.cpp
@@ -2,37 +2,32 @@
 #include "functionality.hpp"
 #include <iostream>
 
-
-
 void acs2_menu_art() {
-    std::cout << "                 ___    ___                                                   \n";
-    std::cout << "                |   \\  |  |   ________   ______    _______    ___   __       \n";
-    std::cout << "                |    \\ |  | /  ______| /   ____\\  /   __   \\  |  \\  | |   \n";
-    std::cout << "                |  \\  \\|  | \\_______   |   /     |   |__|   | | \\ \\ | |  \n";
-    std::cout << "                |  |\\     |   ______ \\ |   \\____ |  |____|  | | |\\ \\| |  \n";
-    std::cout << "                |__| \\____| \\________| \\_______/ |__|    |  | |_| \\___|   \n";
-    std::cout << "-----------------------------------------------------------------------------------------------------\n";
-    std::cout << "-------------------------------Network Scanning Tool By Vulcan Security------------------------------\n";
-    std::cout << "-----------------------------------------------------------------------------------------------------\n";
+    std::cout << "                            ___    ___                                                   \n";
+    std::cout << "                           |   \\  |  |   ________   ______    _______    ___   __       \n";
+    std::cout << "                           |    \\ |  | /  ______| /   ____\\  /   __   \\  |  \\  | |   \n";
+    std::cout << "                           |  \\  \\|  | \\_______   |   /     |   |__|   | | \\ \\ | |  \n";
+    std::cout << "                           |  |\\     |   ______ \\ |   \\____ |  |____|  | | |\\ \\| |  \n";
+    std::cout << "                           |__| \\____| \\________| \\_______/ |__|    |  | |_| \\___|   \n";
+    std::cout << "                   -----------------------------------------------------------------------------\n";
+    std::cout << "                   --------------------Network Scanning Tool By Vulcan Security-----------------\n";
+    std::cout << "                   -----------------------------------------------------------------------------\n";
 }
 
 void hostDescoveryMenu() {
     clear_screen();
     acs2_menu_art();
-
     int choice;
-
     std::cout << "\n=== Host Discovery ===\n";
     std::cout << "1. TCP Ping Sweep\n";
     std::cout << "2. Arp Scan\n";
     std::cout << "3. Back\n";
-    std::cout << "Choose (1-4): ";
-
+    std::cout << "Choose (1-3): ";
     std::cin >> choice;
 
     switch(choice) {
         case 1:
-            tcp_scan();
+            tcpHostDescovery();
             break;
         case 2:
             arpHostDescovery();
@@ -50,16 +45,13 @@ void hostDescoveryMenu() {
 void main_menu() {
     clear_screen();
     acs2_menu_art();
-
     int choice;
-
     std::cout << "\n=== Welcome ===\n";
     std::cout << "1. Host Discovery\n";
     std::cout << "2. Port Scanning\n";
     std::cout << "3. Packet Sniffer\n";
     std::cout << "4. Exit\n";
     std::cout << "Choose option (1-4): ";
-
     std::cin >> choice;
 
     switch(choice) {
@@ -67,7 +59,7 @@ void main_menu() {
             hostDescoveryMenu();
             break;
         case 2:
-            tcp_port_scanner(ip);
+            tcp_port_scanner();
             break;
         case 3:
             packetSniffer();
