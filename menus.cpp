84ba8e448b53395068550745cb8fde1c69ab2c63
Kanax01 <162582100+Kanax01@users.noreply.github.com>
2025-10-31 12:42:29 -0400

    Update menus.cpp

--- menus.cpp
+++ menus.cpp
@@ -2,15 +2,17 @@
 #include "functionality.hpp"
 #include <iostream>
 
+
+
 void acs2_menu_art() {
-    std::cout << " ___    __                                                    \n";
-    std::cout << "|   \\  |  |   ________   ______    _______    ___   __       \n";
-    std::cout << "|    \\ |  | /  ______| /   ____\\  /   __   \\  |  \\  | |   \n";
-    std::cout << "|  \\  \\|  | \\_______   |   /     |   |__|   | | \\ \\ | |  \n";
-    std::cout << "|  |\\     |   ______ \\ |   \\____ |  |____|  | | |\\ \\| |  \n";
-    std::cout << "|__| \\____| \\________| \\_______/ |__|    |  | |_| \\___|   \n";
+    std::cout << "                 ___    ___                                                   \n";
+    std::cout << "                |   \\  |  |   ________   ______    _______    ___   __       \n";
+    std::cout << "                |    \\ |  | /  ______| /   ____\\  /   __   \\  |  \\  | |   \n";
+    std::cout << "                |  \\  \\|  | \\_______   |   /     |   |__|   | | \\ \\ | |  \n";
+    std::cout << "                |  |\\     |   ______ \\ |   \\____ |  |____|  | | |\\ \\| |  \n";
+    std::cout << "                |__| \\____| \\________| \\_______/ |__|    |  | |_| \\___|   \n";
     std::cout << "-----------------------------------------------------------------------------------------------------\n";
-    std::cout << "-------------------------------Network Scanning Tool Vulcan Security---------------------------------\n";
+    std::cout << "-------------------------------Network Scanning Tool By Vulcan Security------------------------------\n";
     std::cout << "-----------------------------------------------------------------------------------------------------\n";
 }
 
@@ -19,7 +21,7 @@ void hostDescoveryMenu() {
     acs2_menu_art();
 
     int choice;
-    
+
     std::cout << "\n=== Host Discovery ===\n";
     std::cout << "1. TCP Ping Sweep\n";
     std::cout << "2. Arp Scan\n";
@@ -33,7 +35,7 @@ void hostDescoveryMenu() {
             tcp_scan();
             break;
         case 2:
-            arp_network_discovery();
+            arpHostDescovery();
             break;
         case 3:
             main_menu();
@@ -41,45 +43,6 @@ void hostDescoveryMenu() {
         default:
             std::cout << "Invalid choice! Try again.\n";
             hostDescoveryMenu();
-            break;
-    }
-}
-
-void portScanningMenu() {
-    clear_screen();
-    acs2_menu_art();
-
-    int choice;
-    std::string ip;
-
-    std::cout << "\n=== Port Scanner ===\n";
-    std::cout << "Enter target IP: ";
-    std::cin >> ip;
-
-    std::cout << "1. Fast Scan\n";
-    std::cout << "2. Custom Port Range\n";
-    std::cout << "3. Custom Port # Scan From File\n";  // Not implemented yet
-    std::cout << "4. Full Port Scan\n";
-    std::cout << "5. Back\n";
-    std::cout << "Pick (1-5): ";
-    std::cin >> choice;
-
-    switch(choice) {
-        case 1:
-        case 2:
-        case 4:
-            tcp_port_scanner(ip);
-            break;
-        case 3:
-            std::cout << "Not Implemented Yet\n";
-            portScanningMenu();
-            break;
-        case 5:
-            main_menu();
-            break;
-        default:
-            std::cout << "Invalid choice!\n";
-            portScanningMenu();
             break;
     }
 }
@@ -93,21 +56,22 @@ void main_menu() {
     std::cout << "\n=== Welcome ===\n";
     std::cout << "1. Host Discovery\n";
     std::cout << "2. Port Scanning\n";
-    std::cout << "3. Map Network\n";
+    std::cout << "3. Packet Sniffer\n";
     std::cout << "4. Exit\n";
     std::cout << "Choose option (1-4): ";
-        
+
     std::cin >> choice;
-        
+
     switch(choice) {
         case 1:
             hostDescoveryMenu();
             break;
         case 2:
-            portScanningMenu();
+            tcp_port_scanner(ip);
             break;
         case 3:
-            mapNetwork();
+            std::cout << "Coming Soon...\n";
+            main_menu();
             break;
         case 4:
             std::cout << "Exiting...\n";
