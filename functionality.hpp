a2469b0eb3fe778457faf2eefc503bbbe63e932c
Kanax01 <162582100+Kanax01@users.noreply.github.com>
2025-10-31 15:51:48 -0400

    Update functionality.hpp

--- functionality.hpp
+++ functionality.hpp
@@ -1,6 +1,5 @@
 #ifndef FUNCTIONALITY_HPP
 #define FUNCTIONALITY_HPP
-
 #include <string>
 #include <vector>
 #include <array>
@@ -11,25 +10,24 @@ struct Device {
 };
 
 // Global Variables
-std::string ip;
+extern std::string ip;
 extern std::array<int, 11> commonPorts;
 extern std::vector<int> livePorts;
 extern std::vector<int> deadPorts;
 extern std::vector<std::string> liveIP;
 extern std::vector<std::string> deadIP;
 extern std::vector<std::string> gateway;
+
 // Functions
 void clear_screen();
 void packetSniffer();
 bool isValidIPv4(const std::string& ip);
-bool isPortOpen(const char* ip, int port); //Why is this not in scope
+bool isPortOpen(const char* ip, int port);
 void tcp_port_scanner(const std::string& ip);
 void show_progress(int current, int total, const std::string& message);
 void tcpHostDescovery();
 void arpHostDescovery();
 std::string getOurIP();
-bool isPortOpen();
 std::string getGatewayIP();
-
 
 #endif // FUNCTIONALITY_HPP
